774c78fa174a3ba4c6a780d833767bf2a6762a8c
Alexies <couchcamote@gmail.com>
2025-02-18 23:43:41 +0800

    - Added tests

--- src/main/java/congestion/calculator/TaxCalculatorApplication.java
+++ src/main/java/congestion/calculator/TaxCalculatorApplication.java
@@ -1,4 +1,4 @@
-package congestion.calculator.controller;
+package congestion.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/congestion/calculator/TaxCalculator/TaxCalculatorApplicationTests.java
+++ src/test/java/congestion/calculator/TaxCalculator/TaxCalculatorApplicationTests.java
@@ -1,13 +1,36 @@
 package congestion.calculator.TaxCalculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.text.ParseException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import congestion.calculator.service.CongestionTaxCalculator;
 
 @SpringBootTest
 class TaxCalculatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	 private CongestionTaxCalculator taxCalculator;
+
+	    @BeforeEach
+	    void setUp() {
+	        taxCalculator = new CongestionTaxCalculator();
+	    }
+
+	    @Test
+	    void testGetTaxForCar() throws ParseException {
+	        String[] timestamps = {
+	            "2013-02-08 06:29:00",  // Expected tax: 8 SEK
+	            "2013-02-08 06:30:27",  // Expected tax: 13 SEK
+	            "2013-02-08 07:15:00"   // Expected tax: 18 SEK
+	        };
+
+	        int tax = taxCalculator.getTaxForVehicle("Car", timestamps);
+
+	        assertEquals(18, tax, "The tax for the car should be 18 SEK.");
+	    }
 
 }
