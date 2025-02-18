85da604a84e148f7b9b16b5ca1ce328c960eba5f
Alexies <couchcamote@gmail.com>
2025-02-19 00:45:48 +0800

    - Added tests

--- src/test/java/congestion/calculator/TaxCalculator/TaxCalculatorApplicationTests.java
+++ src/test/java/congestion/calculator/TaxCalculator/TaxCalculatorApplicationTests.java
@@ -72,16 +72,29 @@ class TaxCalculatorApplicationTests {
 	    }
 	    
 	    @Test
-	    void testGetTax() throws ParseException {
+	    void testGetTaxDifferentDays() throws ParseException {
 	        String[] timestamps = {
 	            "2013-02-08 06:29:00",  // Expected tax: 8 SEK
-	            "2013-02-10 06:30:27",  // Expected tax: 13 SEK
-	            "2013-02-11 07:15:00"   // Expected tax: 18 SEK
+	            "2013-02-11 06:30:27",  // Expected tax: 13 SEK
+	            "2013-02-12 07:15:00"   // Expected tax: 18 SEK
 	        };
 
 	        int tax = taxCalculator.getTaxForVehicle("Car", timestamps);
 
 	        assertEquals(39, tax, "The tax for the car should be 39 SEK.");
+	    }
+	    
+	    @Test
+	    void testGetTaxWithDifferentDaysWithWeekend() throws ParseException {
+	        String[] timestamps = {
+	            "2013-02-08 06:29:00",  // Expected tax: 8 SEK
+	            "2013-02-10 06:30:27",  // Expected tax: 0 SEK  -- weekend
+	            "2013-02-11 07:15:00"   // Expected tax: 18 SEK
+	        };
+
+	        int tax = taxCalculator.getTaxForVehicle("Car", timestamps);
+
+	        assertEquals(26, tax, "The tax for the car should be 26 SEK.");
 	    }
 	    
 
